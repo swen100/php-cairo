b62a4d264787c092242395a2d7fe6817d8316085
Swen Zanon <swen.zanon@yahoo.de>
2019-05-28 08:08:53 +0200

    don't free source buffer

--- src/cairo_jpg.c
+++ src/cairo_jpg.c
@@ -376,7 +376,7 @@ cairo_surface_t *cairo_image_surface_create_from_jpeg_mem(void *data, size_t len
    // set jpeg mime data
    // cairo_surface_set_mime_data(sfc, CAIRO_MIME_TYPE_JPEG, data, len, free, data);
    // don't free data
-   cairo_surface_set_mime_data(sfc, CAIRO_MIME_TYPE_JPEG, data, len, foo, NULL);
+   cairo_surface_set_mime_data(sfc, CAIRO_MIME_TYPE_JPEG, data, len, free, NULL);
 
    return sfc;
 }
